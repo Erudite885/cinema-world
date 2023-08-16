878b4dd7f167e9305ce05aff994b3a9bdb7c1fca
Erudite885 <erudite885@gmail.com>
2023-08-16 06:17:14 +0100

    mui themeing provided

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const theme = createTheme({})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+);
