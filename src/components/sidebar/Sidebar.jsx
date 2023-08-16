2d0354359b04b6be26fb9a79e1c3ee2a1ff195ac
Erudite885 <erudite885@gmail.com>
2023-08-16 17:28:36 +0100

    sidebar folder fixed

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+  ListItemButton,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { useTheme } from "@mui/material/styles";
+import useStyles from "./styles";
+
+const Categories = [
+  { label: "Popular", value: "popular" },
+  { label: "Top Rated", value: "top_rated" },
+  { label: "Upcoming", value: "upcoming" },
+];
+
+const dummyCategory = [
+  { label: "Comedy", value: "comedy" },
+  { label: "Action", value: "action" },
+  { label: "Horror", value: "horror" },
+  { label: "Animation", value: "animation" },
+];
+
+const Sidebar = ({ setMobileOpen }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  //   useEffect(() => {
+  //     first;
+  //   }, [third]);
+
+  return (
+    <>
+      {/* <Link to={`/`} className={classes.imageLink}> */}
+        {/* <img
+          src={theme.palette.mode === "light" ? "" : ""}
+          alt="logo"
+          className={classes.image}
+        /> */}
+        <h2 className={classes.logo}>CINEMA WORLD</h2> {/*temporary, image will come later*/}
+      {/* </Link> */}
+      <Divider />
+      <List>
+        <ListSubheader>Categories </ListSubheader>
+        {Categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItemButton onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img
+                  src={""}
+                  alt=""
+                  className={classes.genreImages}
+                  height={30}
+                />
+              </ListItemIcon> fix img later */}
+              <ListItemText primary={label} />
+            </ListItemButton>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListSubheader>Genres </ListSubheader>
+        {dummyCategory.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItemButton onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img
+                  src={""}
+                  alt=""
+                  className={classes.genreImages}
+                  height={30}
+                />
+              </ListItemIcon> fix img later */}
+              <ListItemText primary={label} />
+            </ListItemButton>
+          </Link>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/sidebar/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  // fix below section
+  logo: {
+    padding: "21px",
+    color: "orange",
+  },
+  // fix above section
+  imageLink: { display: "flex", justifyContent: "center", padding: "10% 0" },
+  image: { width: "70%" },
+  links: { color: theme.palette.text.primary, textDecoration: "none" },
+  genreImages: { filter: theme.palette.mode === "dark" ? "dark" : "invert(1)" },
+}));
