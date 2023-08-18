9caa7ff633bab3d39f89a3796cbc1caf91b60fac
Erudite885 <erudite885@gmail.com>
2023-08-18 08:14:55 +0100

    typo fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Actors } from "./actors/Actors";
 export { default as MovieInfo } from "./movieInfo/MovieInfo";
 export { default as Profile } from "./profile/Profile";
-export { default as Movie } from "./movie/Movie";
+export { default as Movies } from "./movies/Movies";
