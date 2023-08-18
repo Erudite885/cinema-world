85417afe575908402b32bc3970361826b9170aa7
Erudite885 <erudite885@gmail.com>
2023-08-18 09:25:53 +0100

    movie link

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -8,6 +8,19 @@ const Movie = ({ movie, i }) => {
   const classes = useStyles();
   return (
     <Grid className={classes.movie} item xs={12} sm={6} md={4} lg={3} xl={2}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link to={`/movie/${movie.id}`} className={classes.links}>
+          <img
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "https://www.fillmurray.com/200/300"
+            }
+            alt={movie.title}
+            className={classes.image}
+          />
+        </Link>
+      </Grow>
       <Typography className={classes.title} variant="h5">
         {movie.title}
       </Typography>
