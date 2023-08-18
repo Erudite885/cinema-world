3726559a34890f330f5364eeb3c9e4421176e06d
Erudite885 <erudite885@gmail.com>
2023-08-18 08:15:50 +0100

    reducer and middleware added

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { tmdbApi } from "../services/TMDB";
 
 export default configureStore({
-  reducers: {},
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([tmdbApi.middleware]),
 });
