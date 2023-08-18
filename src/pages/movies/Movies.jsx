65c54c9d3c4eeb57dbfee38c9061a5f061306275
Erudite885 <erudite885@gmail.com>
2023-08-18 09:13:31 +0100

    data fetching and if-statement logic

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -10,10 +10,33 @@ import { useGetMoviesQuery } from "../../services/TMDB";
 import { MovieList } from "../../components";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, isFetching, error } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">No Match. Please try again</Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "An error has occured.";
+
   console.log(data);
-  
-  return <div><MovieList movies={data} /></div>;
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
