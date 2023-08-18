bb398a0fe3df926a82d0847ab7e6dc93778c8dc7
Erudite885 <erudite885@gmail.com>
2023-08-18 08:42:52 +0100

    movie folder added

---
+++ src/components/movie/Movie.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Grid, Grow, Rating, Tooltip, Typography } from "@mui/material";
+
+import useStyles from "./styles";
+
+const Movie = ({ movie, i }) => {
+  const classes = useStyles();
+  return <div>Movie</div>;
+};
+
+export default Movie;
