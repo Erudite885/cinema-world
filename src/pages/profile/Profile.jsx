7d01ba2d8fb465db2d2b56dff9e549f9bab8baf7
Erudite885 <erudite885@gmail.com>
2023-08-20 11:37:36 +0100

    profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Box, Button, Typography } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
+
+import { userSelector } from "../../features/auth";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user } = useSelector(userSelector);
 
-export default Profile
+  const favMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favMovies.length ? (
+        <Typography variant="h5">
+          Add Favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>Favorite Movies</Box>
+      )}
+    </Box>
+  );
+};
+
+export default Profile;
