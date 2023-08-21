8b7a24cff072663f03b2616111a1064f87c5bd25
Erudite885 <erudite885@gmail.com>
2023-08-21 11:10:38 +0100

    movie and movielist props imported

--- src/components/movielist/MovieList.jsx
+++ src/components/movielist/MovieList.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Grid } from "@mui/material";
 
 import useStyles from "./styles";
-import { Movie } from "../index";
+import { Movie } from "..";
 
-const MovieList = ({ movies, numberOfMovie }) => {
+const MovieList = ({ movies, numberOfMovies, excludeFirst }) => {
   const classes = useStyles();
 
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.results.slice(0, numberOfMovie).map((movie, i) => (
-        <Movie key={i} movie={movie} i={i} />
-      ))}
+      {movies.results
+        .slice(excludeFirst ? 1 : 0, numberOfMovies)
+        .map((movie, i) => (
+          <Movie key={i} movie={movie} i={i} />
+        ))}
     </Grid>
   );
 };
