5c785e05bdb2154306578bfb97e9b996ee2de156
Erudite885 <erudite885@gmail.com>
2023-08-21 11:08:14 +0100

    typo fixed

--- src/features/auth.js
+++ src/features/auth.js
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+export const authUser = createSlice({
+  name: "authUser",
+  initialState:{
   user: {},
   isAuthenticated: false,
   sessionId: "",
-};
-
-const authSlice = createSlice({
-  name: "user",
-  initialState,
+},
   reducers: {
-    setUser: (state, action) => {
-      state.user = action.payload;
-      state.isAuthenticated = true;
-      state.sessionId = localStorage.getItem("session_id");
+    setUser: (oldState, action) => {
+      oldState.user = action.payload;
+      oldState.isAuthenticated = true;
+      oldState.sessionId = localStorage.getItem("session_id");
 
-      localStorage.setItem("accountId", action.payload.id);
+      localStorage.setItem("account_id", action.payload.id);
     },
   },
 });
 
-export const { setUser } = authSlice.actions;
-export default authSlice.reducer;
-export const userSelector = (state) => state.user;
+export const { setUser } = authUser.actions;
+
+export default authUser.reducer;
+
+export const userSelector = (state) => state.currentUser;
