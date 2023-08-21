138b1cba68cfd3748259ea60b97bfc75df214048
Erudite885 <erudite885@gmail.com>
2023-08-21 11:07:29 +0100

    ratedcards, pagination, featuredmovie imported

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,6 @@ export { default as MovieList } from "./movielist/MovieList";
 export { default as Movie } from "./movie/Movie";
 export { default as Search } from "./search/Search";
 export { default as ActorCredits } from "./actorcredits/ActorCredits";
+export { default as RatedCards } from "./ratedcards/RatedCards";
+export { default as Pagination } from "./pagination/Pagination";
+export { default as FeaturedMovie } from "./featuredmovie/FeaturedMovie";
