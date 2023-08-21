b10c647dea01aa7e39066812ad46a8f2baa409a2
Erudite885 <erudite885@gmail.com>
2023-08-21 10:43:58 +0100

    ToggleColorModeProvider imported and used as wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Provider } from "react-redux";
 
+import App from "./App.jsx";
 import store from "./app/store";
-const theme = createTheme({});
+import ToggleColorModeProvider from "./utils/ToggleColorMode";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <ToggleColorModeProvider>
       <App />
-    </ThemeProvider>
+    </ToggleColorModeProvider>
   </Provider>
 );
