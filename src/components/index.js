38d757616bdf5aa048961d4801721b58be5755cc
Erudite885 <erudite885@gmail.com>
2023-08-21 04:24:42 +0100

    actorcredits added to components

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Sidebar } from "./sidebar/Sidebar";
 export { default as MovieList } from "./movielist/MovieList";
 export { default as Movie } from "./movie/Movie";
 export { default as Search } from "./search/Search";
+export { default as ActorCredits } from "./actorcredits/ActorCredits";
