065309e3d7296e0add763a1aae50fddf681bc01b
Erudite885 <erudite885@gmail.com>
2023-08-25 16:03:26 +0100

    bug-fixed: typography added

--- src/components/ratedcards/RatedCards.jsx
+++ src/components/ratedcards/RatedCards.jsx
@@ -6,8 +6,10 @@ import { Movie } from "..";
 const RatedCards = ({ title, data }) => {
   return (
     <Box>
-      <Typography variant='h5' gutterBottom>{title}</Typography>
-      <Box>
+      <Typography variant="h5" gutterBottom>
+        {title}
+      </Typography>
+      <Box display="flex" flexWrap="wrap">
         {data?.results?.map((movie, i) => (
           <Movie key={movie.id} movie={movie} i={i} />
         ))}
