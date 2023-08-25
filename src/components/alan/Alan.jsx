6896c3a9d220af8ad450b54938939d8ec65eb7d8
Erudite885 <erudite885@gmail.com>
2023-08-25 16:00:58 +0100

    bug-fixed: Alan component fixed

--- src/components/alan/Alan.jsx
+++ src/components/alan/Alan.jsx
@@ -3,8 +3,8 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import { ColorModeContext } from "../../utils/ToggleColorMode";
 import { fetchToken } from "../../utils";
+import { ColorModeContext } from "../../utils/ToggleColorMode";
 import {
   selectGenreOrCategory,
   searchMovie,
@@ -18,34 +18,35 @@ const useAlan = () => {
   useEffect(() => {
     alanBtn({
       key: import.meta.env.VITE_ALAN_KEY,
-      onCommand: ({ command, mode, genreOrCategory, genre, query }) => {
-        if (command === "changeMode") {
-          if (mode === "light") setMode("light");
-          else setMode("dark");
-        } else if (command === "login") {
-          fetchToken();
-        } else if (command === "logout") {
-          localStorage.clear();
-          window.location.href = "/";
-        } else if (command === "chooseGenreOrCategory") {
-          const foundGenre = genre.find(
+      onCommand: ({ command, mode, genreOrCategory, genres, query }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
             (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
           );
 
           if (foundGenre) {
             navigate("/");
             dispatch(selectGenreOrCategory(foundGenre.id));
-          } else if (genreOrCategory) {
+          } else {
             const category = genreOrCategory.startsWith("top")
               ? "top_rated"
               : genreOrCategory;
             navigate("/");
             dispatch(selectGenreOrCategory(category));
           }
+        } else if (command === "changeMode") {
+          if (mode === "light") {
+            setMode("light");
+          } else {
+            setMode("dark");
+          }
+        } else if (command === "login") {
+          fetchToken();
+        } else if (command === "logout") {
+          localStorage.clear();
+          navigate.push("/");
         } else if (command === "search") {
           dispatch(searchMovie(query));
-        } else if (command === "goback") {
-          navigate(-1);
         }
       },
     });
