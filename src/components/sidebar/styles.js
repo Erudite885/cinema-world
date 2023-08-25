6719d65ec8fd3a7fc5abb8f59ba89b943599efbd
Erudite885 <erudite885@gmail.com>
2023-08-25 16:00:07 +0100

    bug-fixed: themeing fixed

--- src/components/sidebar/styles.js
+++ src/components/sidebar/styles.js
@@ -15,5 +15,5 @@ export default makeStyles((theme) => ({
   },
   image: { width: "70%" },
   links: { color: theme.palette.text.primary, textDecoration: "none" },
-  genreImages: { filter: theme.palette.mode === "dark" ? "dark" : "inherit" },
+  genreImages: { filter: theme.palette.mode === "dark" && "invert(1)" },
 }));
